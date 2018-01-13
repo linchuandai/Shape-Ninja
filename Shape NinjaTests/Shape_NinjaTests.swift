88f40280725419fff69ea9418f30a5d684c367f3
Leon Dai <linchuandai@gmail.com>
2018-01-12 23:01:53 -0500

    Initial Commit

---
+++ Shape NinjaTests/Shape_NinjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shape_NinjaTests.swift
+//  Shape NinjaTests
+//
+//  Created by Leon Dai on 2018-01-12.
+//  Copyright © 2018 Leon Dai. All rights reserved.
+//
+
+import XCTest
+@testable import Shape_Ninja
+
+class Shape_NinjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
